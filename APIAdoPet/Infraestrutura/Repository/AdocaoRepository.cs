807a8d9c5a65185f439d2071210863841e455c3e
Jose silvio <josesilvio.bs@gmail.com>
2023-07-05 07:48:55 -0300

    Implementando a AdocaoRepository

--- APIAdoPet/Infraestrutura/Repository/AdocaoRepository.cs
+++ APIAdoPet/Infraestrutura/Repository/AdocaoRepository.cs
@@ -1,6 +1,33 @@
-﻿namespace APIAdoPet.Infraestrutura.Repository
+﻿using APIAdoPet.Domains;
+using APIAdoPet.Domains.Interfaces;
+using APIAdoPet.Infraestrutura.Data;
+
+namespace APIAdoPet.Infraestrutura.Repository
 {
-    public class AdocaoRepository
+    public class AdocaoRepository : IAdocaoRepository
     {
+        private readonly APIAdopetContext _context;
+
+        public AdocaoRepository(APIAdopetContext context)
+        {
+            _context = context;
+        }
+
+        public Adocao CadastrarAdocao(Adocao adocao)
+        {
+            var adocaoCadastrado = _context.Adocaos.Add(adocao);
+            _context.SaveChanges();
+            return adocaoCadastrado.Entity;
+        }
+
+        void IAdocaoRepository.DeletarAdocao(int id)
+        {
+            Adocao? adocao = _context.Adocaos.FirstOrDefault(adocao => adocao.Id == id);
+            if (adocao != null)
+            {
+                throw new Exception("Adoção não existe");
+            }
+            _context.SaveChanges();
+        }
     }
 }
