e52adcb3c15f57c6d039abd829bd6ac0c7cc8fa6
José Silvio <josesilvio.bs@gmail.com>
2023-09-03 13:40:37 -0300

    Implementação dos teste unitários

--- TesteUnitariosAPIAdopet/UnitTest1.cs
+++ TesteUnitariosAPIAdopet/UnitTest1.cs
@@ -1,15 +1,44 @@
+using APIAdoPet.Domains.Interfaces;
 using APIAdoPet.Infraestrutura.Data;
+using APIAdoPet.Infraestrutura.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.DataAnnotations;
+using Xunit.Abstractions;
 
 namespace TesteUnitariosAPIAdopet;
 
 public class UnitTest1
 {
-    [Fact]
-    public void TesteDeConexaoDoBancoDeDados()
+
+    private readonly ITutorRepository _repository;
+
+    private readonly ITestOutputHelper _helper;
+
+    public UnitTest1(ITestOutputHelper helper)
     {
-        var context = new APIAdopetContext(new Microsoft.EntityFrameworkCore.DbContextOptions<APIAdopetContext>());
-        var resultado = context.Database.CanConnect();
-        Assert.True(resultado);
+        var servico = new ServiceCollection();
+        servico.AddTransient<ITutorRepository, TutorRepository>();
+        var provedor = servico.BuildServiceProvider();
+        _repository = provedor.GetService<ITutorRepository>()!;
+        _helper = helper;
+    }
+    [Fact]
+    public void ListarTodosOsTutoresDiferenteDeNull()
+    {
+        var options = new DbContextOptionsBuilder<APIAdopetContext>()
+            .Options;
+        try
+        {
+            var tutores = _repository.ListarTutor(0, 5);
+
+            Assert.NotNull(tutores);
+        }catch(Exception ex)
+        {
+            _helper.WriteLine(ex.Message);
+        }
+        
+        
        
     }
 }
